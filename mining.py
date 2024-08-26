b20af4ebd3dd24679c1644d68aad47e6bcec3441
Zercu <dekakiran540@gmail.com>
2024-08-27 00:43:54 +0530

    Update mining.py

--- mining.py
+++ mining.py
@@ -1,11 +1,11 @@
 import subprocess
-from config import POOL_URL, WALLET_ADDRESS, WORKER_NAME
+from config import POOL_URL, WALLET_ADDRESS, WORKER_NAME, WORKER_PASSWORD
 
 def start_cpu_mining():
     """
     Starts CPU mining using cpuminer on the VPS.
     """
-    command = f"minerd -a scrypt -o {POOL_URL} -O {WALLET_ADDRESS}:{WORKER_NAME} --coinbase-addr={WALLET_ADDRESS} -t 1"
+    command = f"minerd -a scrypt -o {POOL_URL} -O {WORKER_NAME}:{WORKER_PASSWORD} --coinbase-addr={WALLET_ADDRESS} -t 1"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process
 
