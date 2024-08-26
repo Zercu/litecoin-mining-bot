1f721b862994aa780341cbd09c5ded6d4829eae9
Zercu <dekakiran540@gmail.com>
2024-08-27 00:25:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'  # Replace with your Telegram bot token
-ADMIN_IDS = [123456789]            # Replace with your Telegram admin user ID(s)
-BINANCE_API_KEY = 'YOUR_BINANCE_API_KEY'         # Replace with your Binance API key
-BINANCE_API_SECRET = 'YOUR_BINANCE_API_SECRET'   # Replace with your Binance API secret
+TOKEN = '7391908147:AAEV4QRGtFqsAUPwGP09eCCmiDc4N0tzmHQ'  # Replace with your Telegram bot token
+ADMIN_IDS = [6156332908]            # Replace with your Telegram admin user ID(s)
+BINANCE_API_KEY = 'tdoRrOTcnMxXfcGuycifHNuJhxJC2Ij7xOjLOLeOllk58dTWKXDB19Hk7TN9XuGe'         # Replace with your Binance API key
+BINANCE_API_SECRET = ''   # Replace with your Binance API secret
 POOL_URL = 'stratum+tcp://litecoinpool.org:3333' # Example Litecoin pool URL
-WALLET_ADDRESS = 'your_litecoin_wallet_address'  # Your Litecoin wallet address
+WALLET_ADDRESS = 'LhicUG3GNqDV2pNmrByd9e96cQgf6d3Vj4'  # Your Litecoin wallet address
 WORKER_NAME = 'RituRajDeka'                      # Don't change it
