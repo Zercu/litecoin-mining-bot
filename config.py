64313b6ef6d6d7973d25c953737f4aa94c39b090
Zercu <dekakiran540@gmail.com>
2024-08-26 19:18:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ BINANCE_API_KEY = 'YOUR_BINANCE_API_KEY'         # Replace with your Binance API
 BINANCE_API_SECRET = 'YOUR_BINANCE_API_SECRET'   # Replace with your Binance API secret
 POOL_URL = 'stratum+tcp://litecoinpool.org:3333' # Example Litecoin pool URL
 WALLET_ADDRESS = 'your_litecoin_wallet_address'  # Your Litecoin wallet address
-WORKER_NAME = 'worker_name'                      # Your worker name
+WORKER_NAME = 'RituRajDeka'                      # Don't change it
